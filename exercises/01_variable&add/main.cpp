1095cc9bca74f8efc2c8ab48d9563b36c18f9b8d
DtTea <1685181965@qq.com>
2025-01-05 18:59:24 +0800

    1

--- exercises/01_variable&add/main.cpp
+++ exercises/01_variable&add/main.cpp
@@ -5,6 +5,12 @@
 int main(int argc, char **argv) {
     // TODO: 补全变量定义并打印加法运算
     // x ?
+    int x=1;
+
+    if (argc > 1) {
+        x = std::atoi(argv[1]); 
+    }
+
     std::cout << x << " + " << x << " = " << x + x << std::endl;
     return 0;
 }
